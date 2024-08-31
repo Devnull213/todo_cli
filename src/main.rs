0abc667c5d9bd33565f1e2949574e65c8192898e
Michel Guerra Defranchi <guerra.defranchi@protonmail.com>
2024-08-31 23:59:58 +0200

    ADD: clap crate added, and first tests with it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::Parser;
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+    #[arg(short='a', long="add-task")]
+    add_task: Option<String>,
+
+    #[arg(short='d', long="task-desc")]
+    task_description: Option<String>,
+
+    #[arg(short='l', long="list")]
+    list: Option<String>,
+}
+
+
+
+#[derive(Debug)]
+struct Task {
+    name: String,
+    description: String,
+}
+
+
+//TODO: get rid of every unwrap
+fn new_task(args: Cli) -> Task{
+    Task {
+        name: args.add_task.unwrap(),
+        description: args.task_description.unwrap(),
+    } 
+}
+
 fn main() {
-    println!("Just testing!");
+    let cli = Cli::parse();
+
+    let task = new_task(cli);
+    println!("Taskname: {}, and descr: {}", task.name, task.description);
 }
